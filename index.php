8c3f6aa767071fa140d68ce124da859c0c6ccd6d
AswaniNandan <aswani4433@gmail>
2021-01-07 02:43:13 +1400

    bootstrap completed studentapp

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <title>StudentApp</title>
+</head>
+<body>
+    <div class="container">
+
+        <div class="row">
+           <div class="col col-xs-12 col-s-12 col-m-12">
+    <table class="table">
+
+        <tr>
+            <td>Name</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+            <td>Rollno</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+            <td>Admission</td>
+            <td><input type="text" class="form-control"></td>
+        </tr>
+        <tr>
+            <td></td>
+            <td><button class="btn btn-success">Login</button></td>
+        </tr>
+    </table>
+    </div>
+    </div>
+    </div>
+</body>
+</html>
